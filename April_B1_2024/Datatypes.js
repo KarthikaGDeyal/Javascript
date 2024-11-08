92167614c7061aefc356570977871f2614431fd9
KarthikaGDeyal <karthikadeyal1999@gmail.com>
2024-11-08 15:06:17 +0530

    First commit

---
+++ April_B1_2024/Datatypes.js
@@ -0,0 +1,21 @@
+//Numbers
+let num1=20;
+let num2=5;
+let num3=10.5;
+
+//Strings
+let name="Elvin";
+let place="Cochin";
+
+//Boolean
+let a=true;
+let b = false;
+
+//Object
+const Student={Name:"Rahul",Age:25,Place:"Calicut"}
+
+//Array Object
+const course=["Python","MERN","Java"]
+
+//Date Object
+const date=new Date("2024-07-29")
